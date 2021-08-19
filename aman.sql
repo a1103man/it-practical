ab92e34c574ebe324f1bec3045515944bba046a2
a1103man <84636894+a1103man@users.noreply.github.com>
2021-08-19 16:44:20 +0530

    Add files via upload

---
+++ aman.sql
@@ -0,0 +1,16 @@
+CREATE TABLE books (
+   bookid INTEGER PRIMARY KEY,
+   title varchar(100),
+   author varchar(100),
+   price int);
+
+
+
+INSERT INTO books 
+VALUES (1267, "chemistry", "j.l.baird", 2000),
+
+(7689, "Mathematics", "R.D. MISHRA", 2768);
+
+
+
+SELECT * FROM books;
